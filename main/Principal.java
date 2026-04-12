760b63998f8b4ad6b6c0ad883645da1b7bf1e36d
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-04-11 22:31:58 -0300

    Refactor main method and add Moto instance
    Refactored the main method to include a Moto instance and updated output messages.

--- main/Principal.java
+++ main/Principal.java
@@ -1,28 +1,28 @@
 package br.com.fiapdelivery.main;
 
-// Importando as classes que estão no pacote model
 import br.com.fiapdelivery.model.Caminhao;
+import br.com.fiapdelivery.model.Moto;
 import br.com.fiapdelivery.model.Pacote;
 import br.com.fiapdelivery.model.Rota;
 
 public class Principal {
+
     public static void main(String[] args) {
-        System.out.println("--- Sistema FiapDelivery (Refatorado) ---\n");
+        System.out.println("--- Sistema FiapDelivery ---\n");
 
-        // 1. Criação do veículo
         Caminhao caminhao1 = new Caminhao("ABC-1234", 500.0, 4);
-        
-        // 2. Criação do Pacote
         Pacote pacote1 = new Pacote("BR999", 10.5);
+        Rota rota1 = new Rota(pacote1, caminhao1);
 
-        // 3. Montagem da Rota associando o Pacote ao Veículo
-        Rota rotaDeEntrega = new Rota(pacote1, caminhao1);
-        
-        // 4. Executando a entrega
-        rotaDeEntrega.iniciarRota();
-        
-        // Atualizando o status
-        pacote1.atualizarStatus("Em Trânsito");
-        System.out.println("Status atual: " + pacote1.getStatus());
+        rota1.iniciarRota();
+
+        pacote1.atualizarStatus("Em Transito");
+        System.out.println("\nNovo status do pacote: " + pacote1.getStatus());
+
+        System.out.println("\n--- Teste da heranca com Moto ---");
+        Moto moto1 = new Moto("XYZ-9999", 40.0, true);
+        System.out.println("Placa da moto: " + moto1.getPlaca());
+        System.out.println("Capacidade da moto: " + moto1.getCapacidade() + " kg");
+        System.out.println("Possui bau: " + moto1.isPossuiBau());
     }
 }
