02c3dac31933e1c2446b068514713116476bd5ba
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-04-11 22:31:21 -0300

    Update Rota.java

--- model/Rota.java
+++ model/Rota.java
@@ -1,9 +1,9 @@
 package br.com.fiapdelivery.model;
 
 public class Rota {
-    
+
     private Pacote pacote;
-    private Veiculo veiculoDesignado; 
+    private Veiculo veiculoDesignado;
 
     public Rota(Pacote pacote, Veiculo veiculoDesignado) {
         this.pacote = pacote;
@@ -11,7 +11,11 @@ public class Rota {
     }
 
     public void iniciarRota() {
-        System.out.println("📦 Logística FiapDelivery:");
-        System.out.println("Levando pacote [" + this.pacote.getCodigo() + "] no veículo de placa [" + this.veiculoDesignado.getPlaca() + "]");
+        System.out.println("=== FIAP DELIVERY ===");
+        System.out.println("Pacote: " + this.pacote.getCodigo());
+        System.out.println("Peso: " + this.pacote.getPeso() + " kg");
+        System.out.println("Status: " + this.pacote.getStatus());
+        System.out.println("Veiculo designado: " + this.veiculoDesignado.getPlaca());
+        System.out.println("Capacidade: " + this.veiculoDesignado.getCapacidade() + " kg");
     }
 }
